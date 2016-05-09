6d51f716a820c1db4e8cfaf4a2c28824fdc7fa60
Quikky <765pro.gz@gmail.com>
2016-05-09 22:13:24 +0900

    json getまで実装

---
+++ KCAPI/KCAPI_Form.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace KCAPI
+{
+    partial class KCAPI_Form
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LogTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // LogTextBox
+            // 
+            this.LogTextBox.Location = new System.Drawing.Point(12, 152);
+            this.LogTextBox.Multiline = true;
+            this.LogTextBox.Name = "LogTextBox";
+            this.LogTextBox.Size = new System.Drawing.Size(513, 138);
+            this.LogTextBox.TabIndex = 0;
+            // 
+            // KCAPI_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(537, 302);
+            this.Controls.Add(this.LogTextBox);
+            this.Name = "KCAPI_Form";
+            this.Text = "KCAPI";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.KCAPI_Form_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox LogTextBox;
+    }
+}
+
